72bb629234fb37d3b9dc8aac4b834777dff1d422
Ruslan Rutkovsky <rus.rutkovsky@yandex.ru>
2023-05-15 20:58:30 +0300

    commit in new branch "dev"

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           New title
+          New title1
         </p>
         <a
           className="App-link"

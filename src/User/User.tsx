07ed8c3d1e2a1d22bb9f7c4eeb38c76c916be6c0
Ruslan Rutkovsky <rus.rutkovsky@yandex.ru>
2023-05-15 19:31:42 +0300

    add test and user components

---
+++ src/User/User.tsx
@@ -0,0 +1,7 @@
+export const User = () => {
+    return (
+        <div>
+            <h2>User</h2>
+        </div>
+    )
+}
---
+++ src/tests/Test.tsx
@@ -0,0 +1,7 @@
+export const Test = () => {
+    return (
+        <div>
+            <h2>Test</h2>
+        </div>
+    )
+}
